5b473abf501e1d4d08c70f910c968121171b8eb8
varya <varyadaser@gmail.com>
2023-03-03 10:52:34 +0400

    added an object with color values :axe:

--- js/tilemap.js
+++ js/tilemap.js
@@ -5,7 +5,7 @@ const canvas = document.getElementById("cnvs");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
-canvas.height = 400;
+canvas.height = 500;
 
 // set up the tile size and number of columns and rows
 const tileSize = 16;
@@ -13,6 +13,15 @@ const numCols = Math.floor(canvas.width / tileSize);
 const numRows = Math.floor(canvas.height / tileSize);
 
 let tileMap = [];
+
+// set up hex values for colors
+const colors = {
+  grass: "#A0D995",
+  water: "#4CACBC",
+  flower: "#A3A7E4",
+  cat: "",
+  sand: "",
+};
 
 // loop through each tile and randomly assign a tile type
 for (let row = 0; row < numRows; row++) {
@@ -29,17 +38,17 @@ for (let row = 0; row < numRows; row++) {
   for (let col = 0; col < numCols; col++) {
     const tileType = tileMap[row][col];
     let color;
-    if (tileType > 30) {
+    if (tileType > 20) {
       // 30% grass probability
       let ItemInGrass = Math.floor(Math.random() * 100);
       // 10% flower probability in 30% grass
-      if (ItemInGrass < 10) {
-        color = "#A3A7E4";
+      if (ItemInGrass < 12) {
+        color = colors.flower; // purple for flowers
       } else {
-        color = "#A0D995"; // green for grass
+        color = colors.grass; // green for grass
       }
     } else {
-      color = "#4CACBC"; // blue for water
+      color = colors.water; // blue for water
     }
 
     ctx.fillStyle = color;
